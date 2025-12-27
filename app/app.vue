5226a8586abd353e71aaf934d86d2ea9c8f3dfe8
reaz uddin mahmood <reaz.u.mahmood@gmail.com>
2025-12-27 22:54:36 +0100

    setup tailwind, daisyui and icones

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-6xl bg-red-900 font-bold underline">
+      Hello world!
+    </h1>
+
+  </div>
+  <div>
+    Charge
+    <Icon name="tabler:battery-2" style="color: white" size="32" />
   </div>
 </template>
